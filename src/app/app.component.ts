ca6b6ed7bdb11e211cb50e4b6504d3dd09391977
KrishnaBhargaviP <w0866008@myscc.ca>
2024-09-08 20:00:02 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Krishna-Pabbaraju-Learning-Angular';
+
+  // Two new variables with types
+  userName: string = 'Krishna Pabbaraju';
+  userMarks: number = 100;
 }
