7fb037dc501e122b8364f21e5b04482e458777d6
KrishnaBhargaviP <w0866008@myscc.ca>
2024-09-17 17:59:33 -0400

    initialize array with 6 instances of object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,36 @@ export class AppComponent {
     isAdmin: true,
     email: 'w0866008@myscc.ca'
   }
+
+  User2: User = {
+    id : 2,
+    firstName: 'Test user2',
+    lastName: 'last name2',
+    isAdmin: true,
+    email: 'testmail2@myscc.ca'
+  }
+
+  userList: User[] = //any[] would have worked as well
+    [this.User1,this.User2,
+      {id : 3,
+        firstName: 'Test user3',
+        lastName: 'last name3',
+        isAdmin: false,
+        email: 'testmail3@myscc.ca'},
+      {id : 4,
+        firstName: 'Test user4',
+        lastName: 'last name4',
+        isAdmin: true,
+        email: 'testmail4@myscc.ca'},
+      {id : 5,
+        firstName: 'Test user5',
+        lastName: 'last name5',
+        isAdmin: true,
+        email: 'testmail5@myscc.ca'},
+      {id : 6,
+        firstName: 'Test user6',
+        lastName: 'last name6',
+        isAdmin: false,
+        email: 'testmail6@myscc.ca'}
+      ];
 }
