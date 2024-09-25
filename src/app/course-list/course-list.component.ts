8b9a0b94c69f814f21e2596be9a11cd8bce262ed
KrishnaBhargaviP <w0866008@myscc.ca>
2024-09-25 15:34:23 -0400

    add course data array in course-list.component.ts

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -8,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
-
+    courses = [
+      {id: 1,
+        subjectCode: 'MAD001',
+        instructorName: 'Mathew Haug',
+        isAdmin: true,
+        email: 'mathew@myscc.ca',
+        marks: 10},
+      {
+        id: 2,
+        subjectCode: 'MAD002',
+        instructorName: 'Darren',
+        isAdmin: true,
+        email: 'darren@myscc.ca',
+        marks: 10
+      },
+      {id: 3,
+        subjectCode: 'MAD003',
+        instructorName: 'Pantula',
+        isAdmin: true,
+        email: 'pantula@myscc.ca',
+        marks: 10},
+      {id: 4,
+        subjectCode: 'MAD004',
+        instructorName: 'Diana Nano',
+        isAdmin: true,
+        email: 'nano@myscc.ca',
+        marks: 10,},
+      {id: 5,
+        subjectCode: 'MAD005',
+        instructorName: 'Manoj',
+        isAdmin: true,
+        email: 'manoj@myscc.ca',
+        marks: 10},
+      {id: 6,
+        subjectCode: 'MAD006',
+        instructorName: 'Takaki',
+        isAdmin: true,
+        email: 'takaki@myscc.ca',
+        marks: 10}
+    ]
 }
