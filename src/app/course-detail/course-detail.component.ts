a2bed9afde946288abb1686b1e7cf324287fd1a8
KrishnaBhargaviP <w0866008@myscc.ca>
2024-09-25 15:38:13 -0400

    add input property to course detail component

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-course-detail',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './course-detail.component.css'
 })
 export class CourseDetailComponent {
-
+  @Input() course: any;
 }
