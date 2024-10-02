bef69a49c55717545c71750df94a49b5a5714348
KrishnaBhargaviP <w0866008@myscc.ca>
2024-10-01 22:23:48 -0400

    create mock course data and update CRUD

---
+++ src/app/Shared/Modules/mockCourse.data.ts
@@ -0,0 +1,8 @@
+export const courses = [
+  { id: 1, subjectCode: 'MAD001', instructorName: 'Mathew Haug', isAdmin: true, email: 'mathew@myscc.ca', marks: 10 },
+  { id: 2, subjectCode: 'MAD002', instructorName: 'Darren', isAdmin: true, email: 'darren@myscc.ca', marks: 10 },
+  { id: 3, subjectCode: 'MAD003', instructorName: 'Pantula', isAdmin: true, email: 'pantula@myscc.ca', marks: 10 },
+  { id: 4, subjectCode: 'MAD004', instructorName: 'Diana Nano', isAdmin: true, email: 'nano@myscc.ca', marks: 10 },
+  { id: 5, subjectCode: 'MAD005', instructorName: 'Manoj', isAdmin: true, email: 'manoj@myscc.ca', marks: 10 },
+  { id: 6, subjectCode: 'MAD006', instructorName: 'Takaki', isAdmin: true, email: 'takaki@myscc.ca', marks: 10 }
+];
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { courses } from '../Shared/Modules/mockCourse.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  getCourses(): Observable<any[]> {
+    return of(courses);
+  }
 
-  constructor() { }
+  getCourseById(id: number): Observable<any | undefined> {
+    return of(courses.find(course => course.id === id));
+  }
+
+  addCourse(newCourse: any): Observable<any[]> {
+    courses.push(newCourse);
+    return of(courses);
+  }
+
+  updateCourse(updatedCourse: any): Observable<any[]> {
+    const index = courses.findIndex(course => course.id === updatedCourse.id);
+    if (index !== -1) {
+      courses[index] = updatedCourse;
+    }
+    return of(courses);
+  }
+
+  deleteCourse(id: number): Observable<any | undefined> {
+    const index = courses.findIndex(course => course.id === id);
+    const deletedCourse = courses.splice(index, 1)[0];
+    return of(deletedCourse);
+  }
 }
