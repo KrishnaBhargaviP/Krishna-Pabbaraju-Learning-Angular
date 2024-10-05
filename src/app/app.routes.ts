f077e1b66c6543e4baa577f5dfa0f9c1133aa8e0
KrishnaBhargaviP <w0866008@myscc.ca>
2024-10-05 05:55:35 -0400

    create components and add to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ModifyListItemComponent } from './modify-list-item/modify-list-item.component';
 
-export const routes: Routes = [];
+let HomeComponent;
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'modify-list-item', component: ModifyListItemComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
