8d112df8d15525fcb086e55d3246d953599f5576
KrishnaBhargaviP <w0866008@myscc.ca>
2024-10-05 05:57:21 -0400

    create navigation bar in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {JsonPipe, NgForOf} from "@angular/common";
 import {CourseListComponent} from "./course-list/course-list.component";
 import {courses} from "./Shared/Modules/mockCourse.data";
@@ -7,7 +7,7 @@ import {courses} from "./Shared/Modules/mockCourse.data";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, CourseListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, CourseListComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
