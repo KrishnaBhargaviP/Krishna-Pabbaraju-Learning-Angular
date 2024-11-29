b677b745ff85d81d00d1533b16d49ac16a53f984
KrishnaBhargaviP <w0866008@myscc.ca>
2024-11-29 12:09:44 -0500

    add courseList component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatButton} from "@angular/material/button";
+import {JsonPipe, NgForOf} from "@angular/common";
+import {CourseListComponent} from "./course-list/course-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatButton],
+  imports: [RouterOutlet, NgForOf, JsonPipe, CourseListComponent, RouterLink, RouterLinkActive, MatToolbar, MatButton],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
